20346d4c04ac57f9953054d82a15bfaad2791a4d
ivsingh-spec <ivsingh@ucdavis.edu>
2026-02-11 16:11:34 -0800

    Update merge_tables.py

--- merge_tables.py
+++ merge_tables.py
@@ -6,7 +6,7 @@ Load data from the folder F1 Data - take the columns which are relevant and make
 Merge raw F1 CSV tables into a single race-driver dataset.
 Each row represents one driver in one race. Of current drivers only
 """
-
+test
 def merge_tables(dataframes):
     """
     Merges results, races, drivers, constructors, qualifying, and status tables.
